b2ff2a3f1b19a96489c106a1a01ab75bef3b6e59
Steven Brooks <brookscodeblog@gmail.com>
2023-02-21 06:58:33 +0700

    Giving me wrong result so far

---
+++ removeTheMinimum7.js
@@ -0,0 +1,19 @@
+function removeSmallest(numbers) {
+    // set output as array
+    let output = [];
+    // declare variable for storing smallest number
+    let smallNum = numbers[0];
+    // iterate through array
+    for(let number of numbers) {
+        // find smallest number
+        if(number < smallNum) {
+            smallNum = number;
+        }
+    }
+    // remove smallest number and return new array
+    numbers.splice(smallNum);
+    output = numbers;
+    return output;
+  }
+
+  console.log(removeSmallest([9, 4, 3, 3, 7, 2, 4, 1, 7]));
