9b1c9d9841eaaed9a67e20c369a5754ff41d707d
Steven Brooks <brookscodeblog@gmail.com>
2023-02-21 07:31:23 +0700

    Got this nailed in 5 mins after some errors

---
+++ theFeastOfManyBeasts8.js
@@ -0,0 +1,5 @@
+function feast(beast, dish) {
+    return beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length -1) == dish.charAt(dish.length -1);
+  }
+
+  /* MADE A FEW ERRORS BUT CRACKED IN LESS THAN FIVE MINS. */
